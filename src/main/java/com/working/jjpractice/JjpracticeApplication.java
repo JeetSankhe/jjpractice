d97962874fa0cbc32f44072fb0c32ceb065ad099
JeetSankhe <sankhe.jeet@gmail.com>
2022-04-22 12:11:08 +0530

    first commit

---
+++ src/main/java/com/working/jjpractice/JjpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.working.jjpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/working/jjpractice/JjpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.working.jjpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
